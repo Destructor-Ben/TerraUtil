3f3283eeb084e4015ec70023607d6ba57bb665bd
Destructor_Ben <ben.honeyfield@gmail.com>
2024-09-07 19:25:06 +1200

    Initial impl for mod sources symlink

---
+++ src/TerraUtil.BuildSystem/Tasks/GenerateModSourcesSymlink.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Build.Framework;
+
+namespace TerraUtil.BuildSystem.Tasks;
+
+public class GenerateModSourcesSymlink : BaseTask
+{
+    /// <summary>
+    /// The path to the mod's code.
+    /// </summary>
+    [Required]
+    public string ProjectPath { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The path to ModSources.
+    /// </summary>
+    [Required]
+    public string ModSourcesPath { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The internal name of the mod.
+    /// </summary>
+    [Required]
+    public string ModInternalName { get; set; } = string.Empty;
+
+    protected override void Run()
+    {
+        // Check if the mod is already in ModSources
+        string targetPath = Path.Join(ModSourcesPath, ModInternalName);
+        if (Directory.Exists(targetPath))
+            return;
+
+        // Symlink!
+        Directory.CreateSymbolicLink(targetPath, ProjectPath);
+    }
+}
--- src/TerraUtil.BuildSystem/Tasks/GenerateSymlinks.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TerraUtil.BuildSystem.Tasks;
-
-public class GenerateSymlinks : BaseTask
-{
-    protected override void Run()
-    {
-        // TODO: mklink /D "%USERPROFILE%/Documents/My Games/Terraria/tModLoader/ModSources/TerraUtilTestMod" "%CD%"
-    }
-}
