cbac7d7f22d9f95eae25a0d3e0e95e87d2db3897
Destructor_Ben <ben.honeyfield@gmail.com>
2024-11-16 00:15:37 +1300

    Update DevTools.cs

--- src/TerraUtil.DevTools/DevTools.cs
+++ src/TerraUtil.DevTools/DevTools.cs
@@ -5,9 +5,4 @@ namespace TerraUtilDevTools;
 public class DevTools : Mod
 {
     public static DevTools Instance => ModContent.GetInstance<DevTools>();
-
-    public override void Load()
-    {
-        Logger.Debug("Hello!");
-    }
 }
